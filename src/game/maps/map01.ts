eb968cfe16733b28deb67b57b965cbed448a75fd
Nathan <nluciom04@hotmail.com>
2025-09-22 15:56:02 -0300

    feat: add sideBlock to map01 with canGoInside property

--- src/game/maps/map01.ts
+++ src/game/maps/map01.ts
@@ -4,5 +4,8 @@ import MapObject from "../classes/MapObject";
 export default (canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) => {
     const map = new GameMap(canvas, context);
     map.objects.push(new MapObject(canvas, context, 300, 700, 500, 50));
+    const sideBlock = new MapObject(canvas, context, 600, 500, 200, 50);
+    sideBlock.canGoInside = true;
+    map.objects.push(sideBlock);
     return map;
 };
