53656e234cb5080c329150f5b136ef290c25392d
Nathan <nluciom04@hotmail.com>
2025-09-22 14:57:30 -0300

    feat: implement GameMap class for managing game objects and background rendering of maps

---
+++ src/game/classes/Map.ts
@@ -0,0 +1,22 @@
+import Background from "./Background";
+import MapObject from "./MapObject";
+
+export default class GameMap {
+    canvas: HTMLCanvasElement;
+    context: CanvasRenderingContext2D;
+    objects: MapObject[];
+    background: Background;
+    constructor(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
+        this.canvas = canvas;
+        this.context = context;
+
+        this.objects = [];
+        this.background = new Background(this.canvas, this.context);
+    }
+    draw() {
+        this.background.draw();
+        this.objects.forEach((object) => {
+            object.draw();
+        });
+    }
+}
