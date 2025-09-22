a877a68a78ebde757940e4c10776d4c4e7770d12
Nathan <nluciom04@hotmail.com>
2025-09-22 14:56:31 -0300

    feat: implement game initialization and canvas resizing logic

---
+++ src/game/index.ts
@@ -0,0 +1,25 @@
+import Game from "./classes/Game";
+import map01 from "./maps/map01";
+
+const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
+const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+let game: Game;
+
+window.onload = () => {
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+    resizeCanvas();
+
+    game = new Game(canvas, context, map01(canvas, context));
+    window.requestAnimationFrame((currentFrame) => {
+        game.frames.currentFrame = currentFrame;
+        game.draw();
+    });
+
+    window.addEventListener("resize", () => {
+        resizeCanvas();
+    });
+};
