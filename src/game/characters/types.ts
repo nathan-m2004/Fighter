6741ccab6beba08e6384bf1b0c44f48ad93c1c6c
Nathan <nluciom04@hotmail.com>
2025-09-26 23:22:13 -0300

    types

--- src/game/characters/types.ts
+++ src/game/characters/types.ts
@@ -4,23 +4,23 @@ import Fighter from "./Fighter";
 export type Characters = Fighter;
 
 export type Attack = {
-    [key: string]: {
-        x: { current: number; offset: number };
-        y: { current: number; offset: number };
-        width: number;
-        height: number;
-        directional: { bool: boolean; direction: string };
-        keys: { pressed: InputState[]; sideKeysPressed: boolean; notPressed: InputState[] };
-        onGround: { checkFor: boolean; noCheck: boolean };
-        attacking: { bool: boolean; hit: boolean; framesToHitDelta: number };
-        attackKey: InputState;
-        velocity: { x: number; y: number; noChange: boolean };
-        firstFrame: boolean;
-        frameTimeDelta: number;
-        frameLengthDelta: number;
-        frameToUnlockMovementDelta: number;
-        stopMovement: { bool: boolean; frameDelta: number };
-    };
+    x: { current: number; offset: number };
+    y: { current: number; offset: number };
+    width: number;
+    height: number;
+    directional: { bool: boolean; horizontal: string; vertical: string };
+    keys: { pressed: InputState[]; sideKeysPressed: boolean; notPressed: InputState[] };
+    onGround: { checkFor: boolean; noCheck: boolean };
+    hitPoints: number;
+    knockBack: { force: number; stopTime: number };
+    attacking: { bool: boolean; hit: boolean; framesToHitDelta: number };
+    attackKey: InputState;
+    velocity: { x: number; y: number; noChange: boolean };
+    firstFrame: boolean;
+    frameTimeDelta: number;
+    frameLengthDelta: number;
+    frameToUnlockMovementDelta: number;
+    stopMovement: { bool: boolean; frameDelta: number };
 };
 
 export type AttackStates = {
@@ -28,8 +28,10 @@ export type AttackStates = {
     canAttack: boolean;
     attackCooldownDelta: number;
     attackFrameDelta: number;
-    readonly current: string;
-    map: Attack;
+    readonly current: Attack;
+    readonly currentsPositionSize: {};
+    map: { [key: string]: Attack };
+
     hitbox: {
         show: boolean;
     };
