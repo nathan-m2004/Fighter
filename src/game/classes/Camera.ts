2ef2bbe96f08ba2ab5d763e21a30ae5dc0ebb6f5
Nathan <nluciom04@hotmail.com>
2025-10-14 01:25:28 -0300

    feat: Camera follow all

--- src/game/classes/Camera.ts
+++ src/game/classes/Camera.ts
@@ -5,29 +5,56 @@ export default class Camera {
     canvas: HTMLCanvasElement;
     context: CanvasRenderingContext2D;
     position: Position;
-    target: Characters;
+    target: Characters | { position: Position; size: Size };
     size: Size;
-    constructor(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
+    players: Characters[];
+    scale: number;
+    constructor(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D, players: Characters[]) {
         this.canvas = canvas;
         this.context = context;
+        this.players = players;
 
+        this.scale = 1;
         this.position = { x: this.canvas.width / 2, y: this.canvas.height / 2 };
-        this.target;
         this.size = { width: this.canvas.width, height: this.canvas.height };
+        this.target = { position: { x: undefined, y: undefined }, size: { height: undefined, width: undefined } };
     }
     follow(player: Characters) {
         this.target = player;
     }
-    update() {
-        if (!this.target) return;
+    followAll() {
+        let sumX = 0;
+        let sumY = 0;
+        for (let i = 0; i < this.players.length; i++) {
+            sumX += this.players[i].position.x;
+            sumY += this.players[i].position.y;
+        }
 
-        let targetX = this.target.position.x + this.target.size.width / 2 - this.size.width / 2;
-        let targetY = this.target.position.y + this.target.size.height / 2 - this.size.height / 2;
+        const centerX = sumX / this.players.length;
+        const centerY = sumY / this.players.length;
+
+        this.target.position.x = centerX;
+        this.target.position.y = centerY;
+
+        // const farthestPlayer = this.players.slice().sort((a, b) => {
+        //     const distSqA = (a.position.x - centerX) ** 2 + (a.position.y - centerY) ** 2;
+        //     const distSqB = (b.position.x - centerX) ** 2 + (b.position.y - centerY) ** 2;
+        //     return distSqB - distSqA;
+        // });
+    }
+    update() {
+        let targetX;
+        let targetY;
+
+        if (this.target) {
+            targetX = this.target.position.x - this.size.width / 2;
+            targetY = this.target.position.y - this.size.height / 2;
+        }
 
         if (targetX < 0) {
             targetX = 0;
         }
-        if (targetX + this.size.width > this.size.width) {
+        if (targetX > this.size.width) {
             targetX = this.size.width;
         }
 
@@ -35,7 +62,7 @@ export default class Camera {
         if (targetY < 0) {
             targetY = 0;
         }
-        if (targetY + this.size.height > this.size.height) {
+        if (targetY > this.size.height) {
             targetY = this.size.height;
         }
 
@@ -44,6 +71,7 @@ export default class Camera {
     }
     apply() {
         this.context.save();
+        this.context.scale(this.scale, this.scale);
         this.context.translate(-this.position.x, -this.position.y);
     }
     unapply() {
